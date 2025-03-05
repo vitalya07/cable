4266425071304856348f50d4dc55e6aae0478bc8
Vitalii <saifutdinov_vitalii@mail.ru>
2025-03-05 18:29:20 +0500

    page two reade

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+    //маска
+    let selector = document.querySelector('#phone');
+    let im = new Inputmask("+7(999)999-99-99");
+    im.mask(selector);
     //Смена header
     const header = document.querySelector('.header'),
           headerLink = document.querySelectorAll('.header__link'),
