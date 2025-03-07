2477d5ccb06d0df3f07b640b9535e04133e1b2ed
Vitalii <saifutdinov_vitalii@mail.ru>
2025-03-07 20:49:12 +0500

    dfsf

--- js/script.js
+++ js/script.js
@@ -48,7 +48,23 @@ document.addEventListener('DOMContentLoaded', ()=> {
     checkScreenSize();
     window.addEventListener('resize', checkScreenSize);
     changeHeaderBg()
+    //modal
+    const modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('.modal__dialog-close'),
+          modalBtn = document.querySelector('.header__btn');
 
+    function openModal() {
+        modal.classList.add('open')
+    }    
+    function removeModal() {
+        modal.classList.remove('open')
+    }   
+    modalBtn.addEventListener('click',(e)=> {
+        openModal()
+    });
+    modalClose.addEventListener('click', (e)=> {
+        removeModal();
+    });        
     //mobile menu
     const hamburger = document.querySelector('.hamburger'),
           closeMenu = document.querySelector('.header__mobile-close')  
@@ -123,50 +139,104 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     });
    
-   
+    //tabs 
+    const history = document.querySelector('.history');
+    if(history) {
+        const tab = document.querySelectorAll('.history__tab'),
+        content = document.querySelectorAll('.history__item'),
+        tabParrent = document.querySelector('.history__tabs');
+        function hiddenTabs() {
+            tab.forEach(item => {
+                item.classList.remove('tab__active');
+            });
+            content.forEach(item => {
+                item.style.display = 'none'
+            })
+        };
+        function openTabs(i = 0) {
+            tab[i].classList.add('tab__active');
+            content[i].style.display = 'block';
+        }       
+        hiddenTabs();
+        openTabs();
+
+        tabParrent.addEventListener('click', (e)=> {
+            const target = e.target;
+            if(target && target.classList.contains('history__tab')) {
+                tab.forEach((item, i)=> {
+                    if(target == item) {
+                        hiddenTabs();
+                        openTabs(i);
+                    }
+                })
+            }
+        })
+    }
+
+ 
+  
+
     //slider
-    new Splide('#slider1', {
-        type: 'loop',
-        autoplay: 'play',
-        arrows: false,
-        pagination: true,
-        speed: 600,  
-    }).mount();
-    new Splide('#slider2', {
-        type   : 'loop',
-        perPage: 3,
-        perMove: 1,
-        autoplay: 'play',
-        arrows: false,
-        pagination: false,
-        speed: 600,  
-        breakpoints: {
-            990: {
-                perPage: 2,
-            },
-            768: {
-                perPage: 2,
-                gap: '20px'
-            },
-            556: {
-                perPage: 1,
-                gap: 0,
-                padding: '40px'
-            },
-            450: {
-                padding: '10px',
-            },
-            360: {
-                padding: '2px',
-                gap: '10px'
-            },
-        }
-        
-    }).mount();
+    const slider1 = document.querySelector('#slider1'); 
+    if(slider1) {
+        new Splide('#slider1', {
+            type: 'loop',
+            autoplay: 'play',
+            arrows: false,
+            pagination: true,
+            speed: 600,  
+        }).mount();
+    }
+    const slider2 = document.querySelector('#slider2') 
+    if(slider2) {
+        new Splide('#slider2', {
+            type   : 'loop',
+            perPage: 3,
+            perMove: 1,
+            autoplay: 'play',
+            arrows: false,
+            pagination: false,
+            speed: 600,  
+            breakpoints: {
+                990: {
+                    perPage: 2,
+                },
+                768: {
+                    perPage: 2,
+                    gap: '20px'
+                },
+                556: {
+                    perPage: 1,
+                    gap: 0,
+                    padding: '40px'
+                },
+                450: {
+                    padding: '10px',
+                },
+                360: {
+                    padding: '2px',
+                    gap: '10px'
+                },
+            }
+            
+        }).mount();
+    }
+
     //маска
-    let selector = document.querySelector('#phone');
-    let im = new Inputmask("+7(999)999-99-99");
-    im.mask(selector);
+    const form = document.querySelector('form')
+    if(form) {
+        let selector = document.querySelector('#phone');
+        let phone = document.querySelector('#modal-phone');
+        let im = new Inputmask("+7(999)999-99-99");
+        if(selector) {
+            im.mask(selector);
+        }
+        if(phone) {
+            im.mask(phone);
+        }           
+        
+    };  
+       
 })
 
 
